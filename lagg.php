ec461e40f167572c11dd1044e96418561af91ef3
ryan.mcmaster <ryan.mcmaster@CIO54042-12.stone.ne.gov>
2015-08-07 15:26:35 -0500

    Gaming Group Page

---
+++ lagg.php
@@ -0,0 +1,99 @@
+<!DOCTYPE HTML>
+<!--
+        Dopetrope by HTML5 UP
+        html5up.net | @n33co
+        Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+    <head>
+        <title>Lincoln Atheists|LAGG</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/jquery.dropotron.min.js"></script>
+        <script src="js/skel.min.js"></script>
+        <script src="js/skel-layers.min.js"></script>
+        <script src="js/init.js"></script>
+        <script type="text/javascript" src="js/lib/jquery-1.11.1.min.js" ></script>
+        <noscript>
+        <link rel="stylesheet" href="css/skel.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/style-desktop.css" />
+        </noscript>
+        <!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+    </head>
+    <body class="left-sidebar">
+
+        <!-- Header -->
+        <div id="header-wrapper">
+            <div id="header">
+
+                <!-- Logo -->
+                <header>
+                    <h1>Lincoln Atheists Gaming Group</h1>
+                </header>
+                <!-- Nav -->
+                <?php include 'load/nav.php'; ?>
+            </div>
+        </div>
+
+        <!-- Main -->
+        <div id="main-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="4u">
+
+                        <!-- Sidebar -->
+                        <section class="box">
+                            <a href="#" class="image featured"><img src="images/pic09.svg" alt="" /></a>
+                            <header>
+                                <h3>Sed etiam lorem nulla</h3>
+                            </header>
+                            <p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+                            <footer>
+                                <a href="#" class="button alt">Magna sed taciti</a>
+                            </footer>
+                        </section>
+                        <section class="box">
+                            <header>
+                                <h3>Feugiat consequat</h3>
+                            </header>
+                            <p>Veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat sed lorem consequat feugiat lorem dolore.</p>
+                            <ul class="divided">
+                                <li><a href="#">Sed et blandit consequat sed</a></li>
+                                <li><a href="#">Hendrerit tortor vitae sapien dolore</a></li>
+                                <li><a href="#">Sapien id suscipit magna sed felis</a></li>
+                                <li><a href="#">Aptent taciti sociosqu ad litora</a></li>
+                            </ul>
+                            <footer>
+                                <a href="#" class="button alt">Ipsum consequat</a>
+                            </footer>
+                        </section>
+
+                    </div>
+                    <div class="8u important(collapse)">
+
+                        <!-- Content -->
+                        <article class="box post">
+                            <a href="#" class="image featured"><img src="images/pic01.svg" alt="" /></a>
+                            <header>
+                                <h2>Lincoln Atheists Gaming Group</h2>
+                                <p>LAGG</p>
+                            </header>
+                            <p>
+                                A group of godless heathens getting together to play tabletop games. We meet every other week and alternate between Fridays and Saturdays. 
+                            </p>
+                        </article>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Footer -->
+        <?php include 'load/footer.php'; ?>
+
+    </body>
+</html>
